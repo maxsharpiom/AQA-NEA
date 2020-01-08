84cdfd2b426a5fc58e6cbe6cc1055dbfb237a561
maxsharpiom <maxsharp@online.sch.im>
2020-01-08 13:00:45 +0000

    Inventory and Item Class

---
+++ Assets/Scripts/Inventory/Inventory.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Inventory
+{
+    MyList<Items> InventoryList = new MyList<Items>();
+    
+    /// <summary>
+    /// Add an item to the player's inventory
+    /// </summary>
+    /// <param name="item"></param>
+    void AddItem(Item item)
+    {
+        InventoryList.Add(item);        
+    }
+
+    void RemoveItem(Item item)
+    {
+        InventoryList.Remove(item);
+    }
+
+    void SwapItem(Item newItem, Item itemToSwap)
+    {
+        InventoryList.ReplaceNode(newItem, itemToSwap);
+    }
+
+    void AddAmmo(Item item, int amount)
+    {
+        
+    }
+    
+    void RemoveAmmo(Item item, int amount)
+    {
+
+    }
+
+}
---
+++ Assets/Scripts/Items/Item.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class Item : MonoBehaviour
+{
+    string name;
+    bool usableByPlayer;
+    Vector3 position;
+    //May not need
+    string description;
+
+    /// <summary>
+    /// Sets the position of the item in the game world
+    /// </summary>
+    /// <param name="position"></param>
+    public Item(Vector3 position)
+    {
+        this.position = position;
+    }
+
+}
