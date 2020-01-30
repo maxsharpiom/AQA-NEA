783b48bc76e3d63574724e1951cc9ec03ff268df
maxsharpiom <maxsharp@online.sch.im>
2020-01-30 11:18:40 +0000

    Destructable objects, explosion method
    need to add remaing destrucatbvle objects

--- Assets/Scripts/DestructableObject/DestructableObject.cs
+++ Assets/Scripts/DestructableObject/DestructableObject.cs
@@ -4,14 +4,72 @@ using UnityEngine;
 
 public class DestructableObject
 {
-    void Start()
+    protected float maxHealth;
+    protected float currentHealth;
+    protected string name;
+    protected bool explodes;
+    protected bool containsItem;
+    protected Item containingInside; //Might need to be more specific with the type as Item is the base and not the class of the actual item itself
+    protected bool explosionRadius;
+    protected bool maximumDamage; //Damage dealt per meter radius;
+    //Need to referene player for position;
+
+    protected void Update()
     {
 
     }
-    void Update()
-    {
 
+    protected void TakeDamage(float amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            Destroy();
+        }
     }
+
+    void Destroy()
+    {
+        if (this.explodes == true)
+        {
+            Explode();
+        }
+        if (this.containsItem == true)
+        {
+            //Drop containing item
+            DropContainingItem();
+        }
+
+        //Remove gameObject from world
+        Object.Destroy(this.gameObject);
+    }
+
+    void Explode()
+    {
+        float dealDamageToSourounding;
+        //Returns true if there are any colliders overlapping the sphere
+        //https://docs.unity3d.com/ScriptReference/Physics.OverlapSphere.html
+        //https://docs.unity3d.com/ScriptReference/GameObject.SendMessage.html
+        //https://docs.unity3d.com/ScriptReference/Vector3.Distance.html?_ga=2.121006024.1588713239.1580308064-324497535.1560950234
+        Collider[] explosionHit = Physics.OverlapSphere(this.position, explosionRadius);
+        float distanceToPlayerSquared;
+
+        //Inverse square law
+        for (int i = 0; i < explosionHit.Length; i++)
+        {
+            distanceToPlayerSquared = Vector3.distance(explosionHit.position, this.transform.position);
+            dealDamageToSourounding = maximumDamage * (1 / distanceToPlayerSquared);
+            explosionHit.SendMessage("TakeDamage", dealDamageToSourounding);
+        }
+        //play explosion anim
+    }
+
+    void DropContainingItem()
+    {
+        //Spawn item in and set position equal to the destructable object
+        Instantiate(containsItem, this.transform.position);
+    }
+
 }
 
 
---
+++ Assets/Scripts/DestructableObject/MediumCrate.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MediumCrate : DestructableObject
+{
+
+    void Start()
+    {
+        this.maxHealth = 20f;
+        this.currentHealth = maxHealth;
+        this.name = "ExplosiveBarrel";
+    }
+
+    public ExplosiveBarrel(Vector3 position)
+    {
+        this.position = position;
+    }
+
+}
+
+
